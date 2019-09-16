05d716435880ad8845e25d5adb00b2e46748cc24
Jaroslav Hanslík <kukulich@kukulich.cz>
2019-09-16 14:49:24 +0200

    Added urban areas

--- layers/landcover/landcover.sql
+++ layers/landcover/landcover.sql
@@ -9,8 +9,9 @@
 --);
 --CREATE INDEX IF NOT EXISTS landcover_grouped_gen2_geometry_idx ON landcover_grouped_gen2 USING gist(geometry);
 
-CREATE OR REPLACE FUNCTION landcover_class(landuse VARCHAR, "natural" VARCHAR, leisure VARCHAR, wetland VARCHAR) RETURNS TEXT AS $$
+CREATE OR REPLACE FUNCTION landcover_class(landuse VARCHAR, "natural" VARCHAR, leisure VARCHAR, wetland VARCHAR, urban VARCHAR) RETURNS TEXT AS $$
     SELECT CASE
+        WHEN urban = 'urban' THEN 'urban'
         WHEN landuse IN ('farmland', 'farm', 'orchard', 'vineyard', 'plant_nursery') THEN 'farmland'
         WHEN "natural" IN ('glacier', 'ice_shelf') THEN 'ice'
         WHEN "natural"='wood' OR landuse IN ('forest') THEN 'wood'
@@ -28,63 +29,69 @@ $$ LANGUAGE SQL IMMUTABLE;
 
 -- etldoc: ne_110m_glaciated_areas ->  landcover_z0
 CREATE OR REPLACE VIEW landcover_z0 AS (
-    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_110m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, NULL::text as urban FROM ne_110m_glaciated_areas
 );
 
 CREATE OR REPLACE VIEW landcover_z2 AS (
     -- etldoc: ne_50m_glaciated_areas ->  landcover_z2
-    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_50m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, NULL::text as urban FROM ne_50m_glaciated_areas
     UNION ALL
     -- etldoc: ne_50m_antarctic_ice_shelves_polys ->  landcover_z2
-    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_50m_antarctic_ice_shelves_polys
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, NULL::text as urban FROM ne_50m_antarctic_ice_shelves_polys
+    UNION ALL
+    -- etldoc: ne_50m_urban_areas ->  landcover_z2
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, NULL::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, 'urban'::text AS urban FROM ne_50m_urban_areas
 );
 
 CREATE OR REPLACE VIEW landcover_z5 AS (
     -- etldoc: ne_10m_glaciated_areas ->  landcover_z5
-    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_10m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, NULL::text as urban FROM ne_10m_glaciated_areas
     UNION ALL
     -- etldoc: ne_10m_antarctic_ice_shelves_polys ->  landcover_z5
-    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_10m_antarctic_ice_shelves_polys
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, NULL::text as urban FROM ne_10m_antarctic_ice_shelves_polys
+    UNION ALL
+    -- etldoc: ne_10m_urban_areas ->  landcover_z2
+    SELECT NULL::bigint AS osm_id, geometry, NULL::text AS landuse, NULL::text AS "natural", NULL::text AS leisure, NULL::text AS wetland, 'urban'::text AS urban FROM ne_10m_urban_areas
 );
 
 CREATE OR REPLACE VIEW landcover_z7 AS (
     -- etldoc: osm_landcover_polygon_gen7 ->  landcover_z7
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen7
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen7
 );
 
 CREATE OR REPLACE VIEW landcover_z8 AS (
     -- etldoc: osm_landcover_polygon_gen6 ->  landcover_z8
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen6
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen6
 );
 
 CREATE OR REPLACE VIEW landcover_z9 AS (
     -- etldoc: osm_landcover_polygon_gen5 ->  landcover_z9
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen5
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen5
 );
 
 CREATE OR REPLACE VIEW landcover_z10 AS (
     -- etldoc: osm_landcover_polygon_gen4 ->  landcover_z10
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen4
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen4
 );
 
 CREATE OR REPLACE VIEW landcover_z11 AS (
     -- etldoc: osm_landcover_polygon_gen3 ->  landcover_z11
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen3
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen3
 );
 
 CREATE OR REPLACE VIEW landcover_z12 AS (
     -- etldoc: osm_landcover_polygon_gen2 ->  landcover_z12
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen2
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen2
 );
 
 CREATE OR REPLACE VIEW landcover_z13 AS (
     -- etldoc: osm_landcover_polygon_gen1 ->  landcover_z13
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen1
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon_gen1
 );
 
 CREATE OR REPLACE VIEW landcover_z14 AS (
     -- etldoc: osm_landcover_polygon ->  landcover_z14
-    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland, NULL::text as urban FROM osm_landcover_polygon
 );
 
 -- etldoc: layer_landcover[shape=record fillcolor=lightpink, style="rounded, filled", label="layer_landcover | <z0_1> z0-z1 | <z2_4> z2-z4 | <z5_6> z5-z6 |<z7> z7 |<z8> z8 |<z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13|<z14_> z14+" ] ;
@@ -92,7 +99,7 @@ CREATE OR REPLACE VIEW landcover_z14 AS (
 CREATE OR REPLACE FUNCTION layer_landcover(bbox geometry, zoom_level int)
 RETURNS TABLE(osm_id bigint, geometry geometry, class text, subclass text) AS $$
     SELECT osm_id, geometry,
-        landcover_class(landuse, "natural", leisure, wetland) AS class,
+        landcover_class(landuse, "natural", leisure, wetland, urban) AS class,
         COALESCE(
             NULLIF("natural", ''), NULLIF(landuse, ''),
             NULLIF(leisure, ''), NULLIF(wetland, '')
