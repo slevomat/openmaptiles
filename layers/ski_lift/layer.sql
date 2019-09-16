6408ba7a6d5de93dffde4c03f550c5afc1d17170
Jaroslav Hanslík <kukulich@kukulich.cz>
2019-09-16 21:38:54 +0200

    Ski lifts and ski areas

---
+++ layers/ski_lift/layer.sql
@@ -0,0 +1,24 @@
+-- etldoc: layer_ski_lift[shape=record fillcolor=lightpink, style="rounded,filled",
+-- etldoc:     label="layer_ski_lift | <z12_> z12+" ] ;
+
+CREATE OR REPLACE FUNCTION layer_ski_lift(bbox geometry, zoom_level integer)
+RETURNS TABLE(
+		osm_id bigint,
+		geometry geometry,
+		name text
+	) AS $$
+    SELECT
+        osm_id,
+        geometry,
+        name
+    FROM (
+        -- etldoc: osm_skilift_linestring -> layer_ski_lift:z12_
+        SELECT
+            osm_id,
+	        geometry,
+	        name
+	    FROM osm_skilift_linestring
+           WHERE zoom_level >= 12
+    ) AS zoom_levels
+    WHERE geometry && bbox;
+$$ LANGUAGE SQL IMMUTABLE;
